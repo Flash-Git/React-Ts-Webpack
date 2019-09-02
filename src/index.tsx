1bfd17644c063818b1bcc2573f2e48b2e0cb67c7
Flash <flash.jaq@gmail.com>
2019-09-03 01:01:38 +0200

    Added state

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,42 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
 import ReactDOM from "react-dom";
 
+interface ITodos {
+  text: string;
+  complete: boolean;
+}
+
 const App = (): JSX.Element => {
-  return <h1>Hello</h1>;
+  const [value, setValue] = useState<string>("");
+  const [todos, setTodos] = useState<ITodos[]>([]);
+
+  type FormElem = React.FormEvent<HTMLFormElement>;
+
+  const submit = (e: FormElem): void => {
+    e.preventDefault();
+    addTodo(value);
+    setValue("");
+  };
+
+  const addTodo = (text: string) => {
+    const newTodos: ITodos[] = [...todos, { text, complete: false }];
+    setTodos(newTodos);
+  };
+
+  return (
+    <Fragment>
+      <h1>Hello</h1>
+      <form onSubmit={submit}>
+        <input
+          type="test"
+          value={value}
+          onChange={e => setValue(e.target.value)}
+          required
+        />
+        <button type="submit">Add</button>
+      </form>
+    </Fragment>
+  );
 };
 
 const root = document.getElementById("root");
